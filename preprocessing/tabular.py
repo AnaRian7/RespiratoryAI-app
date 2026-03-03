b613440addc205331f8728aff667da9198e12b52
AnaRian7 <156986548+AnaRian7@users.noreply.github.com>
2026-03-03 14:19:11 -0600

    Delete preprocessing directory

--- preprocessing/tabular.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def preprocess_tabular(data: dict):
-    return np.array([
-        data["age"],
-        data["smoker"],
-        data["asthma"],
-        data["genetics"]
-    ], dtype="float32")
